10da321d230ca8e436fb2dc151a3f1a70b92bd2e
Babken <126095475+BabHovhannisyan2010@users.noreply.github.com>
2023-02-26 13:29:46 +0400

    Add files via upload

---
+++ Pizza/app.js
@@ -0,0 +1,19 @@
+let header = document.querySelector("header")
+window.addEventListener("scroll",function () {
+    header.classList.toggle ("sticky" , window.scrollY > 100)
+})
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x') //poxrkel
+    navlist.classList.toggle('open')
+};
+
+
+window.onscroll= () => {
+    menu.classList.remove('bx-x')   //heracnel
+    navlist.classList.remove('open')
+};
+
